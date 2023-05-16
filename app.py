c52d03d3cb9e248dd3dabf7d1bc0efcfc765cf1b
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-16 11:18:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 st.title("this is the app title")
+
+st.sidebar.tile("This is written inside the sidebar")
+st.sidebar.button("Click")
+st.sidebar.radio("Pick your gender", ["Male", "Female"])
