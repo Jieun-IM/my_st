cf6212a513f8de37356af555eab8a48aa79b1005
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-16 11:18:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.write(x, 'squared is', x * x)
 
 st.title("this is the app title")
 
-st.sidebar.tile("This is written inside the sidebar")
+st.sidebar.title("This is written inside the sidebar")
 st.sidebar.button("Click")
 st.sidebar.radio("Pick your gender", ["Male", "Female"])
