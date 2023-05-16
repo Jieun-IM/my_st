2659d97ac423922d83210c6bccee336284517c4c
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-17 01:57:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,53 @@ import numpy as np
 st.title("this is the app title")
 st.header("this is the markdown")
 st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''a+ar^1+ar^2+ar^3''')
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+st.subheader("Image :")
+st.image("kid.jpg")
+
+st.subheader("Audio :")
+st.audio("Audio.mp3")
+
+st.subheader("Video :")
+st.video("video.mp4")
+
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick your gender', ['Male', 'Female'])
+st.multiselect('choose a planet', ['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0.50)
+
+st.number_input('Pick a number', 0, 10)
+st.text_input('Email address')
+st.date_input('Traveling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favourite color')
 
 st.sidebar.title("This is written inside the sidebar")
 st.sidebar.button("Click")
 st.sidebar.radio("Pick your gender", ["Male", "Female"])
 
+container = st.container()
+container.write("This is written inside the container")
+st.write("This is written outside the container")
+
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
+
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
+
+df = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['x', 'y'])
+st.line_chart(df)
