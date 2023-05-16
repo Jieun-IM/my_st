5731b73e850774b2d12d89895ce7e457efe8f98f
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-16 11:30:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 st.title("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
 
 st.sidebar.title("This is written inside the sidebar")
 st.sidebar.button("Click")
